0389a49fece8860c4335cb0484e952711568e560
returnaction <obergannv@gmail.com>
2025-09-11 17:51:08 +0300

    add untracked files

---
+++ delivery-service/src/test/java/com/nikita/deliveryservice/DeliveryEntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikita.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryEntityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
